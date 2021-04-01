1d3a1dc15f7c12932e63b068cc66a4e1fbd734ce
Onur AKIN <39294809+nikaruno@users.noreply.github.com>
2021-04-01 11:42:35 +0300

    Create combi.cpp
    For string vector combination

---
+++ Implementation/combi.cpp
@@ -0,0 +1,5 @@
+    for (vector<string>::iterator it = topic.begin(); it != topic.end(); it++){
+        for (vector<string>::iterator it2 = it+1; it2 != topic.end(); it2++){
+            //TO DO  
+        }
+    }
